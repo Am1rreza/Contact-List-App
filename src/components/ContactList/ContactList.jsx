f928c731b9a94a26fba87865c08b89134c5a0c74
Amirreza <amirreza.m13825@gmail.com>
2023-02-06 21:47:38 +0330

    contact list styles added + conditional rendering added

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,23 @@
 import styles from "./contactList.module.css";
 
 const ContactList = ({ contacts, onDelete }) => {
+  if (contacts.length === 0) return <h2>Add some contact !</h2>;
+
   return (
-    <section>
+    <section className={styles.contactList}>
       {contacts.map((contact) => {
         const { name, email, id } = contact;
 
         return (
-          <div key={id}>
-            <p>
-              <span>Name:</span> {name}
-            </p>
-            <p>
-              <span>Email:</span> {email}
-            </p>
+          <div key={id} className={styles.contact}>
+            <div>
+              <p>
+                <span>Name:</span> {name}
+              </p>
+              <p>
+                <span>Email:</span> {email}
+              </p>
+            </div>
             <button className="btn secondary" onClick={() => onDelete(id)}>
               Delete
             </button>
